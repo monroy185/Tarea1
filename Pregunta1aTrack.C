81dec6b4323bfe61782300912f1dbeca9a45025b
monroy185 <m.monroy@pucp.edu.pe>
2019-04-23 20:44:47 -0500

    primer commit

---
+++ Pregunta1aTrack.C
@@ -0,0 +1,11 @@
+#include "TH1.h"
+#include "TTree.h"
+
+void Pregunta1aTrack(){
+
+TTree *T = new TTree("T","data");
+Long64_t nlines = T->ReadFile("NuAstro_4yr_IceCube_Data.txt","ID:DepEnergy:Errmin:Errmax:Time:Declination:RAdeg:MedAngResdeg:Topology");
+TH1F *h1 = new TH1F("h1","x",1,-100,100);
+
+T->Draw("MedAngResdeg","Topology==2");
+}
